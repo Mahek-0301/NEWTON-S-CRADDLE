c88635d9f7ff674701d0d5cb837fed16cb5bdd42
Mahek-0301 <74814697+Mahek-0301@users.noreply.github.com>
2020-12-20 23:39:06 +0530

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,26 @@
+class Rope{
+    constructor(bodyA,bodyB,offsetX,offsetY){
+        this.offsetX = offsetX;
+        this.offsetY = offsetY;
+        var options = {
+            bodyA : bodyA,
+            bodyB : bodyB,
+            pointB:{x:this.offsetX,y:this.offsetY}
+
+        }
+
+        this.rope = Constraint.create(options)
+        World.add(world,this.rope)
+    }
+
+    display(){
+        var pointA = this.rope.bodyA.position;
+        var pointB = this.rope.bodyB.position;
+        strokeWeight(2);
+
+        var anchorX = pointB.x + this.offsetX;
+        var anchorY = pointB.y + this.offsetY;
+
+         line(pointA.x,pointA.y,anchorX,anchorY);
+    }
+}
